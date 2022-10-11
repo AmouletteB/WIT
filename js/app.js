5b48afa8edb0fe464b4d904b984c94e6e3b98b8a
AmouletteB <amelie.barrau@gmail.com>
2022-10-11 14:22:09 +0100

    fixed bugs

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.getElementById("signup").addEventListener("click", function(){
+    alert("Thank you!")
+});
+
+function submit() {
+    let form = document.getElementById("form__submit");
+    form.submit();
+}
