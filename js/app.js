1d6e546eddf64784cc8ad3b88e988acd0f5fda45
AmouletteB <amelie.barrau@gmail.com>
2022-10-12 11:28:19 +0100

    sign up

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ document.getElementById("signup").addEventListener("click", function (event) {
 });
 
 
-document.getElementById("submit").addEventListener("click", function (event) {
+document.getElementById("signup").addEventListener("click", function (event) {
     event.preventDefault ();
     document.getElementById("welcome").style.display = "block";
-    document.getElementById("submitform").style.display = "none";
+    document.getElementById("signupform").style.display = "none";
 });
 
